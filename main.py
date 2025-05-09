677d0ba0511cafb633103fa4d73fdd6a334356f7
GhosTHaise <ghostrex2@gmail.com>
2025-05-09 20:18:39 +0300

    feat : prompt template

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-def main():
-    print("Hello from small-ai-agent!")
+from langchain_ollama.llms import OllamaLLM
+from langchain_core.prompts import ChatPromptTemplate
 
+model = OllamaLLM(model="llama3.2:1b")
 
-if __name__ == "__main__":
-    main()
+template = """
+    You are an expert in answering questions about a pizza restaurent
+    
+    Here are some relevant reviews: {reviews}
+    
+    Here is the question to answer: {question}
+"""
+
+prompt = ChatPromptTemplate(template)
