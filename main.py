96796e09cec49e3182b5823abf249d3ba7843611
GhosTHaise <ghostrex2@gmail.com>
2025-05-09 21:35:34 +0300

    feat : do request

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from langchain_ollama.llms import OllamaLLM
 from langchain_core.prompts import ChatPromptTemplate
 
-model = OllamaLLM(model="llama3.2:1b")
+OLLAMA_IP = "http://172.24.192.1:11434"
+model = OllamaLLM(model="llama3.2:1b", base_url=OLLAMA_IP)
+
 
 template = """
     You are an expert in answering questions about a pizza restaurent
@@ -11,4 +13,13 @@ template = """
     Here is the question to answer: {question}
 """
 
-prompt = ChatPromptTemplate(template)
+prompt = ChatPromptTemplate.from_template(template)
+chain = prompt | model
+
+result = chain.invoke({
+    "reviews" : [],
+    "question" : "What is the best pizza place in town ?"
+})
+
+print(result)
+
