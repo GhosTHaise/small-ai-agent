30071dec3bff109272868036050cc167896dd80f
GhosTHaise <ghostrex2@gmail.com>
2025-05-10 16:33:42 +0300

    feat : get reviews relative to the question

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from langchain_ollama.llms import OllamaLLM
 from langchain_core.prompts import ChatPromptTemplate
 from config import OLLAMA_IP
+from vector import retriever
 
 model = OllamaLLM(model="llama3.2:1b", base_url=OLLAMA_IP)
-
 
 template = """
     You are an expert in answering questions about a pizza restaurent
@@ -22,8 +22,10 @@ while True:
     if question == "q":
         break
     
+    reviews = retriever.invoke(question)
+    
     result = chain.invoke({
-        "reviews" : [],
+        "reviews" : reviews,
         "question" : question
     })
 
