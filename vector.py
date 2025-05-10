28d4a015e048418606de0c1feac85d121839c1aa
GhosTHaise <ghostrex2@gmail.com>
2025-05-10 16:30:00 +0300

    feat : create chrome db

--- vector.py
+++ vector.py
@@ -26,4 +26,11 @@ if add_documents:
         ids.append(str(i))
         documents.append(document)
         
+vector_store = Chroma(
+    collection_name="restaurant_reviews",
+    persist_directory=db_location,
+    embedding_function=embeddings
+)
 
+if add_documents:
+    vector_store.add_documents(documents=documents, ids=ids)
